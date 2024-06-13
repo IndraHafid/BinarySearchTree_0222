1d1ff75327e8a43dd0a59863fae09d28dbf168bd
IndraHfid <indrahafidsaputra@gmail.com>
2024-06-13 16:02:39 +0700

    Make the lift child of the parent point to the new node

--- BinarySearchTree/BinarySearchTree.cpp
+++ BinarySearchTree/BinarySearchTree.cpp
@@ -32,21 +32,25 @@ public:
     void insert(string element) //Insert a node in in the binary search tree
     {
         Node* newNode = new Node(element, nullptr, nullptr); //Allocate memory for the new node
-    newNode->info = element; //Assign value to the data field of the new node
-    newNode->leftchiId = nullptr;//Make the left child of the new node point to NULL.
-    newNode->rightchiId = nullptr; //Make the right child of the new node point to NULL.
+        newNode->info = element; //Assign value to the data field of the new node
+        newNode->leftchiId = nullptr;//Make the left child of the new node point to NULL.
+        newNode->rightchiId = nullptr; //Make the right child of the new node point to NULL.
 
-    Node* parent = nullptr;
-    Node* currentNode = nullptr;
-    search(element, parent, currentNode); //Locate the node wich will be the parent of the node to be insert
+        Node* parent = nullptr;
+        Node* currentNode = nullptr;
+        search(element, parent, currentNode); //Locate the node wich will be the parent of the node to be insert
 
-    if (parent == nullptr) //If the parent is NULL(Tree is empty)
-    {
-        ROOT = newNode; //Mark the new node as ROOT.
-        return; //Exit
-    }
+        if (parent == nullptr) //If the parent is NULL(Tree is empty)
+        {
+            ROOT = newNode; //Mark the new node as ROOT.
+            return; //Exit
+        }
 
-   
+        if (element < parent->info) //If the value in the data field of the new node is les than that of the
+        {
+            parent->leftchiId = newNode; //Make the lift child of the parent point to the new node
+        }
+       
 };
 
 int main()
